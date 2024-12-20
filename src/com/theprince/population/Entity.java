d648be61451cebf00fd0568b6e649b222169e965
cataclasmic <eric.holloway.aws@gmail.com>
2024-12-20 15:12:20 -0500

    finished basic rules

--- src/com/theprince/population/Entity.java
+++ src/com/theprince/population/Entity.java
@@ -2,7 +2,7 @@ package com.theprince.population;
 
 public class Entity {
 	public double purse;
-	public double radical;
+	public double radical; // TODO this should cover conservative as well
 	public double conservative;
 	public Entity(double purse, double radical, double conservative) {
 		this.purse = purse;
