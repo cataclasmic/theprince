c413e966d42780954f6967eba216f11cc78cfe20
cataclasmic <eric.holloway.aws@gmail.com>
2024-12-20 11:02:24 -0500

    stuff

--- theprince.h
+++ theprince.h
@@ -1,6 +1,23 @@
+#include <list>
+
 class Peasant
 {
  public:
   void deal(Peasant p);
   int money;
+};
+
+class Attribute
+{
+};
+
+class Event
+{
+};
+
+class Pop
+{
+ public:
+  int size;
+  int id;
 };
